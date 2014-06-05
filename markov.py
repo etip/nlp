0a585e2ace717c663410ecac146d2b1cf324a790
Petia <petia@ubuntu.(none)>
2014-06-05 21:05:35 +0100

    Fix the formatting of the output.

--- markov.py
+++ markov.py
@@ -6,19 +6,25 @@ import random
 everything={}
 while True:
 	a = raw_input("Write text here:").lower()
-	tokens = ["START"]
-	tokens.extend(nltk.word_tokenize(a))
-	bigrams = nltk.bigrams(tokens)
-	for bigram in bigrams:
-		if bigram[0] in everything.keys():
-			everything[bigram[0]].append(bigram[1])
-		else:
-			everything[bigram[0]]=[bigram[1]]
-	current_token = "START"	
+	if a:
+		tokens = ["START"]
+		tokens.extend(nltk.word_tokenize(a))
+		tokens.append("END")
+		bigrams = nltk.bigrams(tokens)
+		for bigram in bigrams:
+			if bigram[0] in everything.keys():
+				everything[bigram[0]].append(bigram[1])
+			else:
+				everything[bigram[0]]=[bigram[1]]
+	current_token = "START"
+	final_sentence_components = []	
 	while current_token in everything.keys():
 		possibilities = everything[current_token]
 		current_token = random.choice(possibilities)
-		print current_token
+		final_sentence_components.append(current_token)
+		final_sentence_components[0]=final_sentence_components[0].title()
+ 	print	' '.join(final_sentence_components[:-1]) + "."
+
 			
 
 
